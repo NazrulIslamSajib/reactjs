5f2b932709a396a877b8e9c25a80493fe2d9a44b
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-06-26 16:15:34 +0600

    Create class6.jsx

---
+++ class6.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode> 
+      <div>   
+          <div className="card">
+           
+           <h1>hello sajib</h1>
+
+          </div>
+      </div>
+  </React.StrictMode>,
+) 
+
+/*
+css code
+.card
+{
+    background-color: #35d32f;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(22, 185, 177, 0.1);
+    overflow: hidden;
+    width: 300px;
+    margin: 20px; 
+    display: inline-block;
+    padding: 10px 15px;
+    background-color: #007BFF;
+    color: #fff;
+    text-align: center;
+    border-radius: 4px;
+    text-decoration: none;
+    font-size: 1em;
+}
+
+  */
