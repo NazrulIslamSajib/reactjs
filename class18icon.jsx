ae99359841ea296dc46fcf7c307896ef38e55024
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-06-28 00:26:11 +0600

    Create class18icon.jsx

---
+++ class18icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BsArrowThroughHeart } from "react-icons/bs";
+export default function Card1() {
+  return (
+    <div>
+      <h1>sajib</h1> 
+      <button> Button </button> 
+      <BsArrowThroughHeart/>
+    </div>
+  )
+}
